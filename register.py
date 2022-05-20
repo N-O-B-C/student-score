9b34b4b71b9a9e7501760f24bc8a9c459cdc8912
Nnadi onyebuchi <90341262+N-O-B-C@users.noreply.github.com>
2022-05-20 10:10:50 +0100

    Create register.py

---
+++ register.py
@@ -0,0 +1,12 @@
+from student import Student
+
+name = input("\nEnter name for new Student: ")
+age = input("Enter age for new student: ")
+gender = input("Enter student gender: ")
+
+test_student = Student(name, age, gender)
+print(f"\nHello! My name is {test_student.name} and I'm {test_student.age} years old.")
+
+test_student.enter_score()
+
+print(f'\nSubjects for {test_student.name} are:\n{test_student.subjects}',)
